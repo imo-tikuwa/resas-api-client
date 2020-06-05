3ef88e77df176ecb309223ebf9783aedeafd3ddf
imo-tikuwa <imotikuwabathclean@gmail.com>
2020-06-05 18:29:03 +0900

    迷走してるのでブランチ切っていったんプッシュ

--- src/Api/ResasApiClient.php
+++ src/Api/ResasApiClient.php
@@ -36,6 +36,22 @@ class ResasApiClient {
 	}
 
 	/**
+	 * Destructor
+	 */
+	public function __destruct()
+    {
+        if ($this->last) {
+            return $this->_call_api();
+        }
+    }
+
+	/**
+	 * メソッドチェインの最後か判定
+	 * @link https://qiita.com/ngyuki/items/93075c5472c3417ff355
+	 */
+	private $last = false;
+
+	/**
 	 * RESAS-APIの利用登録時に発行されたAPIキー
 	 * @var string
 	 */
@@ -54,20 +70,57 @@ class ResasApiClient {
 	private $parameters;
 
 	/**
+	 * レスポンスのタイプ
+	 * @var string
+	 */
+	private $response_type;
+
+	/**
+	 * Hashクラスで絞り込む際のkey側のパス構文
+	 * @var string
+	 */
+	private $key_path = null;
+
+	/**
+	 * Hashクラスで絞り込む際のvalue側のパス構文
+	 * @var string
+	 */
+	private $value_path = null;
+
+	/**
+	 * リクエストする際のアクション をセット
+	 */
+	private function set_response_type($response_type)
+	{
+		$this->response_type = $response_type;
+		return $this;
+	}
+
+	/**
+	 * Hash::combine()でkey側として使用する値のパス構文 をセット
+	 */
+	private function set_key_path($key_path)
+	{
+		$this->key_path = $key_path;
+		return $this;
+	}
+
+	/**
+	 * Hash::combine()でvalue側として使用する値のパス構文 をセット
+	 */
+	private function set_value_path($value_path)
+	{
+		$this->value_path = $value_path;
+		return $this;
+	}
+
+	/**
 	 * リクエストする際のアクション をセット
 	 */
 	public function set_action($action)
 	{
 		$this->action = $action;
 		return $this;
-	}
-
-	/**
-	 * リクエストする際のアクション を取得
-	 */
-	public function get_action()
-	{
-		return $this->action;
 	}
 
 	/**
@@ -78,30 +131,21 @@ class ResasApiClient {
 		$this->parameters = $parameters;
 		return $this;
 	}
-
-	/**
-	 * リクエストする際の検索条件 を取得
-	 */
-	public function get_parameters()
-	{
-		return $this->parameters;
-	}
-
 	/**
 	 * RESASにcurlでAPIリクエストする
 	 * @return array
 	 */
 	private function _call_api()
 	{
-		$action = $this->get_action();
-		if (is_null($action)) {
+		if (is_null($this->response_type)) {
+			throw new Exception("Please call the api response_type methond.");
+		} else if (is_null($this->action)) {
 			throw new Exception("Please set the request action of RESAS-API");
 		}
-		$parameters = $this->get_parameters();
 
-		$url = self::API_ENDPOINT . $action;
-		if (!is_null($parameters)) {
-			$url .=  "?" . http_build_query($parameters);
+		$url = self::API_ENDPOINT . $this->action;
+		if (!is_null($this->parameters)) {
+			$url .=  "?" . http_build_query($this->parameters);
 		}
 
 		$curl = curl_init();
@@ -118,6 +162,7 @@ class ResasApiClient {
 		$response = curl_exec($curl);
 		curl_close($curl);
 
+		// レスポンスのチェック
 		if ($response == '"400"') {
 			throw new Exception("400 Bad Request");
 		}
@@ -128,7 +173,28 @@ class ResasApiClient {
 			throw new Exception("Response result not found.");
 		}
 
+		// レスポンスタイプを元にレスポンスを整形
+		if ($this->response_type === 'json') {
+			return json_encode($response['result']);
+		} else if ($this->response_type === 'object') {
+			return json_decode(json_encode($response['result']));
+		} else if ($this->response_type === 'kv_array') {
+			return Hash::combine($response['result'], $this->key_path, $this->value_path);
+		}
 		return $response['result'];
+	}
+
+	/**
+	 * メソッドチェインの最後判定フラグをtrueに代えた$thisのクローンを返す
+	 * @link https://qiita.com/ngyuki/items/93075c5472c3417ff355
+	 * @return $this
+	 */
+	private function ret()
+	{
+		$this->last = false;
+        $obj = clone $this;
+        $obj->last = true;
+        return $obj;
 	}
 
 	/**
@@ -146,40 +212,48 @@ class ResasApiClient {
 
 	/**
 	 * 結果を配列で取得
-	 * @return array
+	 * @return $this
 	 */
-	public function to_array()
+	public function set_response_type_to_array()
 	{
-		return $this->_call_api();
+		$this->set_response_type('array');
+		$this->set_key_path(null);
+		$this->set_value_path(null);
+		return $this;
 	}
 
 	/**
 	 * 結果をjson文字列で取得
-	 * @return string
+	 * @return $this
 	 */
-	public function to_json()
+	public function set_response_type_to_json()
 	{
-		$response = $this->_call_api();
-		return json_encode($response);
+		$this->set_response_type('json');
+		$this->set_key_path(null);
+		$this->set_value_path(null);
+		return $this->ret();
 	}
 
 	/**
 	 * 結果をオブジェクトで取得
-	 * @return object
+	 * @return $this
 	 */
-	public function to_obj()
+	public function set_response_type_to_object()
 	{
-		$response = $this->_call_api();
-		return json_decode(json_encode($response));
+		$this->set_response_type('object');
+		$this->set_key_path(null);
+		$this->set_value_path(null);
+		return $this->ret();
 	}
 
 	/**
 	 * 結果をKey Valueのペアに持ち替えた配列で取得する
 	 * ※配列の持ち替えについてCakePHP3のUtility/Hashクラスを使用しています
-	 * @param string $key_path key側として使用する値のパス構文
-	 * @param string $value_path value側として使用する値のパス構文
+	 * @param string $key_path Hash::combine()でkey側として使用する値のパス構文(必須)
+	 * @param string $value_path Hash::combine()でvalue側として使用する値のパス構文(必須)
+	 * @return $this
 	 */
-	public function to_kv_array($key_path = null, $value_path = null)
+	public function set_response_type_to_kv_array($key_path = null, $value_path = null)
 	{
 		if (is_null($key_path)) {
 			throw new Exception('$key_path is required.');
@@ -187,30 +261,37 @@ class ResasApiClient {
 		if (is_null($value_path)) {
 			throw new Exception('$value_path is required.');
 		}
-		$response = $this->_call_api();
-		$response = Hash::combine($response, $key_path, $value_path);
-		return $response;
+		$this->response_type = 'kv_array';
+		$this->set_key_path($key_path);
+		$this->set_value_path($value_path);
+		return $this->ret();
+	}
+
+	/**
+	 * 明示的なAPIリクエスト実行
+	 * @return string|array
+	 */
+	public function call() {
+		$this->last = false;
+		return $this->_call_api();
 	}
 
 	/**
 	 * 結果をファイルに再利用可能な形でエクスポート
 	 * @param string $filename ファイル名
-	 * @param string $under_php54 trueのとき配列の形式を[]からphp5.4未満のバージョンで読み込み可能なarray()に変更
 	 * @return int|false
 	 */
-	public function export_to($filename = null, $under_php54 = false)
+	public function export_to($filename = null)
 	{
 		$response = $this->_call_api();
 		$out  = "<?php" . PHP_EOL;
 		$out .= "return ";
 		$export = var_export($response, true);
-		if (!$under_php54) {
-			$export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
-			$export = preg_replace("/    /", "\t", $export);
-			$array = preg_split("/\r\n|\n|\r/", $export);
-			$array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
-			$export = join(PHP_EOL, array_filter(["["] + $array));
-		}
+		$export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
+		$export = preg_replace("/    /", "\t", $export);
+		$array = preg_split("/\r\n|\n|\r/", $export);
+		$array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
+		$export = join(PHP_EOL, array_filter(["["] + $array));
 		$out .= $export;
 		$out .= ";" . PHP_EOL;
 		return file_put_contents($filename, $out);
